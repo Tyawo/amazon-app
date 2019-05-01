1cdc944f1263c62281cb14dc367694d2626bee0e
Tyawo <48452575+Tyawo@users.noreply.github.com>
2019-04-30 21:55:09 -0400

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,39 +1,27 @@
-// Include the node-geocoder and inquirer NPM packages
+// Include the mysql and inquirer NPM packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+// reating the connection to the database
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Lnf2mpetj",
+  password: "root",
   database: "bamazon_db"
 });
 
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
+  console.log("connected as id " + connection.threadId + "\n");  
 
-  console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.log(res);
-      console.log("\n-----------------------------------")
-  afterConnection();
-
-     
-
-});
-var afterConnection = function () {
-  // Prompt the user to provide item information.
+  // // Prompt the user to provide item information.
   inquirer.prompt([
 
     {
       type: "input",
       name: "item_id",
       message: "What's your item_id?",
-     
     },
 
     {
@@ -41,60 +29,6 @@ var afterConnection = function () {
       name: "quantity",
       message: "How many units do you want to buy?"
     },
-
-
-
-    // After the prompt, store the user's response in a variable called response.
-  ]).then(function (user) {
-    connection.query("SELECT stock_quantity FROM products", function (err, res) {
-      if (user.quantity > stock_quantity) {
-        console.log("Insufficient quantity")
-      }
-      else {
-        console.log("Fulfilling your order...\n");
-        connection.query("SELECT stock_quantity FROM products", function (err, res) {
-        });
-     }
-
-
-    }),
-
-
-
-
-
-
-      // run queries here
-
-      connection.end();
-
-  });
-}
-
-
-
-
-
-
-
-// Replace with your mapquest consumer API key
-// var options = {
-//   provider: "mapquest",
-//   apiKey: "itAS4yqnwTOtoclOiaxxQAhZFKAAsbwG"
-// };
-
-// var geocoder = NodeGeocoder(options);
-
-
-
-
-//   //   // console.log(location.userInput);
-
-//   //   // Then use the Google Geocoder to Geocode the address
-//   //   geocoder.geocode(location.userInput, function(err, data) {
-
-//   //     console.log(JSON.stringify(data, null, 2));
-//   //   });
-
-// });
-  });
+       // After the prompt, store the user's response in a variable called response.
+  ])
+ 
